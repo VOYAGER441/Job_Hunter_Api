4e3b183d7e135258f456703c4e42eaa5ce592b02
VOYAGER441 <maianka407@gmail.com>
2026-07-07 18:20:53 +0530

    feat: Enhance resume building process with credit validation and cache invalidation

--- src/services/resume.service.ts
+++ src/services/resume.service.ts
@@ -8,12 +8,26 @@ import { AppError } from "@/error/AppError";
 import utils from "@/utils";
 import { IResumeCreateRequest } from "@/interface/request/resume.request";
 import userService from "./user.service";
+import redisService from "./redis.service";
+
+const RESUME_BUILD_CREDIT_COST = 5;
 
 class ResumeService {
 
     // build resume
     async buildResume(userId: mongoose.Types.ObjectId): Promise<string> {
         Log.info("ResumeService::::buildResume:::: called");
+
+        const user = await userService.getUserById(userId);
+        if (!user) {
+            throw new AppError("No user found for this userId", utils.http.HttpStatusCodes.NOT_FOUND);
+        }
+
+        const creditsRemaining = (user.totalCredits || 0) - (user.creditsUsed || 0);
+        if (creditsRemaining < RESUME_BUILD_CREDIT_COST) {
+            throw new AppError("Insufficient credits. Please purchase more credits.", utils.http.HttpStatusCodes.BAD_REQUEST);
+        }
+
         const resumeDoc = await this.getResumeByUser(userId);
 
         if (!resumeDoc) {
@@ -25,6 +39,12 @@ class ResumeService {
 
         try {
             const response = await axios.get<string>(`${env.RESUME_SERVER_URL}/v2/resume/generate/${resumeDoc._id}`);
+
+            await userModel.getDBModel().findByIdAndUpdate(userId, { $inc: { creditsUsed: RESUME_BUILD_CREDIT_COST } });
+
+            const cacheKey = `${utils.cacheConstant.USER_CACHE_KEY_PREFIX}:${userId}`;
+            await redisService.delete(cacheKey);
+            Log.info("ResumeService::::buildResume:::: credits decremented and cache invalidated", { userId });
 
             Log.info("ResumeService::::buildResume:::: resume buffer created successfully for userId: " + userId);
             return response.data;
@@ -44,6 +64,11 @@ class ResumeService {
             throw new AppError("No user found for this userId", utils.http.HttpStatusCodes.NOT_FOUND);
         }
 
+        const creditsRemaining = (user.totalCredits || 0) - (user.creditsUsed || 0);
+        if (creditsRemaining < RESUME_BUILD_CREDIT_COST) {
+            throw new AppError("Insufficient credits. Please purchase more credits.", utils.http.HttpStatusCodes.BAD_REQUEST);
+        }
+
         const resumeDoc = await this.getResumeByUser(userId);
         Log.info("ResumeService::::finalBuildResume:::: resumeDoc " + JSON.stringify(resumeDoc._id))
         try {
@@ -52,10 +77,16 @@ class ResumeService {
             
             const response = await axios.post<ArrayBuffer>(`${env.RESUME_SERVER_URL}/v2/resume/generateAndStore/${resumeDoc._id}`,  {htmlData} );
 
-            // update the resume counter in the database in user db 
+            // update the resume counter and decrement credits in the database
             if (response.status === 200) {
                 Log.info("ResumeService::::finalBuildResume:::: resume generated successfully for userId: " + userId);
-                await userModel.getDBModel().findByIdAndUpdate(userId, { $inc: { resumeCount: 1 } }); // increment resumeCount by 1
+                await userModel.getDBModel().findByIdAndUpdate(userId, { 
+                    $inc: { resumeCount: 1, creditsUsed: RESUME_BUILD_CREDIT_COST } 
+                });
+
+                const cacheKey = `${utils.cacheConstant.USER_CACHE_KEY_PREFIX}:${userId}`;
+                await redisService.delete(cacheKey);
+                Log.info("ResumeService::::finalBuildResume:::: credits decremented and cache invalidated", { userId });
             }
 
             Log.info("ResumeService::::buildResume:::: resume buffer created successfully for userId: " + userId);
